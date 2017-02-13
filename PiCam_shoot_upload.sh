d60511be00b8f3c768e0c7a8cfbccfddbbd608d2
Martin Koksrud Bekkelund <martin@Martins-MacBook-Pro.local>
2017-02-13 19:47:26 +0100

    Updated with english descriptions

--- PiCam_shoot_upload.sh
+++ PiCam_shoot_upload.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Tar et bilde og lagrer det
+# EN: Shoot and save (see manual: https://www.raspberrypi.org/documentation/raspbian/applications/camera.md)
+# NO: Tar et bilde og lagrer det (se manual: https://www.raspberrypi.org/documentation/raspbian/applications/camera.md)
 raspistill -sh 50 -sa 30 -q 60 -n -o /home/pi/Pictures/pi.jpg
 
-# Laster opp bildet
+# EN: Upload to server
+# NO: Laster opp bildet
 scp /home/pi/Pictures/pi.jpg martin@example.com:/public_html/pi.jpg
 
-# Sletter bildet lokalt
+# EN: Delete locally
+# NO: Sletter bildet lokalt
 rm /home/pi/Pictures/pi.jpg
--- PiCam_time_lapse.sh
+++ PiCam_time_lapse.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Sjekksum for å se om filen er endret siden sist
+# EN: Checksum to see if file has changed
+# NO: Sjekksum for å se om filen er endret siden sist
 md5sum -c --status $HOME/public_html/pi.md5
 
 if [ $? -eq 1 ]
 then
-	# Filen er endret, ta en kopi
+	# EN: File is changed, make a copy
+	# NO: Filen er endret, ta en kopi
 	cp $HOME/public_html/pi.jpg $HOME/public_html/log/$(date +%Y-%m-%d_%H%M%S).jpg
-	# Oppdater sjekksum
+	# EN: Update checksum
+	# NO: Oppdater sjekksum
 	md5sum -b $HOME/public_html/pi.jpg > $HOME/public_html/pi.md5
 fi
